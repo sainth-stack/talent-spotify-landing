c4196c057b1c783304c57fae5c6f44813d56da9b
paramesh-reddy <gurakaparameshwerreddy@gmail.com>
2024-11-09 12:22:04 +0530

    careerr-changes

--- pages/careers.js
+++ pages/careers.js
@@ -1,28 +1,41 @@
-import React, { useState } from 'react';
-import Footer from '../Components/Footer';
-import image1 from '.././assets/images/pricing1.png'
-import image2 from '.././assets/images/pricing2.png'
-import image3 from '.././assets/images/pricing3.png'
-import Card3 from '../Components/Card3';
-import BlogNav from '../Components/BlogNav';
-import TermsConditions from '../Components/TermsConditions';
-import Careers from '../Components/Careers';
-
+import React, { useState } from "react";
+import Footer from "../Components/Footer";
+import image1 from ".././assets/images/pricing1.png";
+import image2 from ".././assets/images/pricing2.png";
+import image3 from ".././assets/images/pricing3.png";
+import Card3 from "../Components/Card3";
+import BlogNav from "../Components/BlogNav";
+import TermsConditions from "../Components/TermsConditions";
+import Careers from "../Components/Careers";
 
 export default function Career() {
   const [showPopup, setShowPopup] = useState(false);
 
   return (
-    <div className='bg-white bgCareer'>
-      <div className='container'>
-        <BlogNav showPopup={showPopup} setShowPopup={() => setShowPopup(false)} heading="Careers " subheading="To amplify human potential and create the next opportunity for people, businesses and communities." />
+    <div className="bg-white bgCareer">
+      <div className="container">
+        <BlogNav
+          showPopup={showPopup}
+          setShowPopup={() => setShowPopup(false)}
+        />
+        {/* <div className="Carer-heading">
+          <div><h1>Work With Us!</h1>
+        
+        <p>
+          To amplify human potential and create the <br />
+          next oppurtunity for people,businesses and communities.
+        </p>
+        <button>View Job Openings</button></div>
+      
+       <div>
+        <img  className="career-image"src={image3} alt="image"/>
+       </div>
+       </div> */}
       </div>
-      <div className='d-flex justify-content-center mt-lg-5 pt-lg-5 flex-wrap'>
+      <div className="d-flex justify-content-center mt-lg-5 pt-lg-5 flex-wrap">
         <Careers />
       </div>
       <Footer />
     </div>
-  )
-
-
+  );
 }
