79f5d6af1f9a7fb3345dfc5f7f99b96970ceb82c
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-12-29 11:48:52 +0530

    "done"

--- Components/section3/Section2Section2OKR.js
+++ Components/section3/Section2Section2OKR.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import image1 from "../../assets/images/pedict.png"; // Assuming the correct path to your image
+import image3 from "../../assets/images/left_key_result.png"; // Assuming the correct path to your image
 import image2 from "../../assets/images/pedict.png"; // Assuming the correct path to your image
-import image3 from "../../assets/images/pedict.png"; // Assuming the correct path to your image
+import image1 from "../../assets/images/Right_KeyResult.png"; // Assuming the correct path to your image
 import Image from "next/image";
 import ListPoints from "../ListPoints";
 
-export const Section2OKR = ({ reverse = true, showPopup, homerefScroll }) => {
+export const Section2OKR = ({ reverse = false, showPopup, homerefScroll }) => {
   const [isHover, setIsHover] = useState(false);
 
   const handleHover = () => {
@@ -18,19 +18,19 @@ export const Section2OKR = ({ reverse = true, showPopup, homerefScroll }) => {
 
   return (
     <div
-      className="d-flex align-items-center w-full justify-content-center mt-4 mb-4 pb-4"
+      className="d-flex flex-wrap sm:mx-20 md:mx-0 align-items-center justify-content-center mt-4 mb-4 pb-4 pt-4 "
+      style={{ gap: "15%", margin: "auto", flexDirection: "row" }}
       ref={homerefScroll}
     >
       {/* Image Content Section */}
       <div
-        className={`image-container imagePic m-0 p-0 ${
+        className={`image-container  m-0 p-0 ${
           reverse ? "reverse" : ""
         }`}
         onMouseEnter={handleHover} // Trigger hover effect
         onMouseLeave={handleMouseLeave} // Trigger hover out effect
       >
         <div className="image-wrapper ">
-          {/* Main image */}
           <Image
             src={image2} // Main image
             alt="Base Image"
@@ -40,12 +40,18 @@ export const Section2OKR = ({ reverse = true, showPopup, homerefScroll }) => {
           />
 
           {/* Show additional images on hover */}
-          <div className={`hover-images-container ${isHover ? "visible" : ""}`}>
+          <div
+            className={`hover-images-container ${isHover ? "visible" : ""} 
+          
+         
+
+          `}
+          >
             <div className="hover-top-right">
               <Image
                 src={image1}
                 alt="Top Right Image"
-                width={170}
+                width={150}
                 height={200}
                 className="corner-image"
               />
@@ -55,7 +61,7 @@ export const Section2OKR = ({ reverse = true, showPopup, homerefScroll }) => {
               <Image
                 src={image3}
                 alt="Bottom Left Image"
-                width={380}
+                width={270}
                 height={150}
                 className="corner-image"
               />
@@ -68,7 +74,7 @@ export const Section2OKR = ({ reverse = true, showPopup, homerefScroll }) => {
       <div className="text-content" style={{ maxWidth: "50%", width: "30%" }}>
         <ListPoints
           heading="AI-powered OKR Management"
-          subheading="Everything that you need to achieve your goal in one place."
+          // subheading="Everything that you need to achieve your goal in one place."
           list1={[
             "Track your OKRs",
             "Collaborate effectively",
@@ -77,10 +83,6 @@ export const Section2OKR = ({ reverse = true, showPopup, homerefScroll }) => {
           showPopup={showPopup}
           link="/more-info"
         />
-
-       
-      
-       
       </div>
     </div>
   );
