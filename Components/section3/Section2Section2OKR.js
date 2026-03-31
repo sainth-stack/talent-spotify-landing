d60ad5c81fc2ade4dbfc384e375d1a8ec66059d8
sainath-stylishop <sainath@stylishop.com>
2026-03-31 17:51:55 +0530

    fixed import error

--- Components/section3/Section2Section2OKR.js
+++ Components/section3/Section2Section2OKR.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import image3 from "../../assets/images/left_key_result.png"; // Assuming the 
 import image2 from "../../assets/images/pedict.png"; 
-import image1 from "../../assets/images/right_keyresult.png";
+import image1 from "../../assets/images/Right_keyResult.png";
 import Image from "next/image";
 import ListPoints from "../ListPoints";
 
