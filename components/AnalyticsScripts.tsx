f883704b14d16886762095c7656cd5fdf0932a7f
sainth-stack <123sainathreddy100@gmail.com>
2026-06-29 21:37:42 +0530

    updated analytics

--- components/AnalyticsScripts.tsx
+++ components/AnalyticsScripts.tsx
@@ -5,6 +5,7 @@ export function AnalyticsScripts() {
   return (
     <>
       <Script src="/clarity.js" strategy="afterInteractive" />
+      <Script src="/faittracker.js" strategy="afterInteractive" />
       <Script
         src="https://tag.clearbitscripts.com/v1/pk_dc97a33b14aa2e9ebc805625c1f38492/tags.js"
         strategy="afterInteractive"
